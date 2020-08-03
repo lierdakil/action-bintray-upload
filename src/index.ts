fb6ac1ee1df7079e454e941ebb6f43702ef665b9
Nikolay Yakimov <root@livid.pp.ru>
2020-08-03 19:58:46 +0300

    Slightly better error handling

--- src/index.ts
+++ src/index.ts
@@ -15,38 +15,54 @@ async function run() {
   const versionVcsTag = core.getInput('versionVcsTag')
   const filesGlob = core.getInput('filesGlob')
 
-  const bt = new Bintray(username, apikey, subject)
-  const br = bt.repository(repository)
-  await br.info()
-  const bp = br.package(pkg)
   try {
-    await bp.info()
+    const bt = new Bintray(username, apikey, subject)
+    const br = bt.repository(repository)
+    await br.info()
+    const bp = br.package(pkg)
+    try {
+      console.log('Checking for package...')
+      await bp.info()
+    } catch (e) {
+      if (!createPkg) throw e
+      console.error(e)
+      console.log((await e.responseBody).toString())
+      console.log("Package doesn't seem to exist, creating...")
+      await bp.create({
+        licenses: core.getInput('licenses').split(',') as any[],
+        vcs_url: `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`,
+      })
+    }
+    const bv = bp.version(version)
+    try {
+      console.log('Checking for version...')
+      await bv.info()
+    } catch (e) {
+      console.error(e)
+      console.log((await e.responseBody).toString())
+      console.log('Version does not seem to exist, creating...')
+      await bv.create({
+        desc: versionDescription || '',
+        vcs_tag: versionVcsTag || undefined,
+      })
+    }
+    console.log('Gathering assets...')
+    const gfiles = await new Promise<string[]>((resolve, reject) => {
+      glob(filesGlob, (error, result) => {
+        if (error) reject(error)
+        else resolve(result)
+      })
+    })
+    console.log('Uploading assets...')
+    await Promise.all(gfiles.map((f) => bv.uploadContent(f)))
+    console.log('Publishing revision...')
+    await bv.publishContent()
   } catch (e) {
-    if (!createPkg) throw e
+    console.log('Failed')
     console.error(e)
-    await bp.create({
-      licenses: core.getInput('licenses').split(',') as any[],
-      vcs_url: `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`,
-    })
+    console.log((await e.responseBody).toString())
+    core.setFailed(e.message)
   }
-  const bv = bp.version(version)
-  try {
-    await bv.info()
-  } catch (e) {
-    console.error(e)
-    await bv.create({
-      desc: versionDescription || '',
-      vcs_tag: versionVcsTag || undefined,
-    })
-  }
-  const gfiles = await new Promise<string[]>((resolve, reject) => {
-    glob(filesGlob, (error, result) => {
-      if (error) reject(error)
-      else resolve(result)
-    })
-  })
-  await Promise.all(gfiles.map((f) => bv.uploadContent(f)))
-  await bv.publishContent()
 }
 
 run().catch((err) => core.setFailed(err.message))
